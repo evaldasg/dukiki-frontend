b2bcc8eaef26d039de1793e0e8c02afe37fe9d83
Evaldas Grudzinskas <prohdo@icloud.com>
2017-01-18 22:21:49 +0200

    Add all pages with initial markup

---
+++ js/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Match } from 'react-router'
+import MainPage from './MainPage'
+import Profile from './Profile'
+import Budgets from './Budgets'
+import Expenses from './Expenses'
+import Settings from './Settings'
+
+const App = React.createClass({
+  render () {
+    return (
+      <BrowserRouter>
+        <div className='app'>
+          <Match exactly pattern='/' component={MainPage} />
+          <Match exactly pattern='/profile' component={Profile} />
+          <Match exactly pattern='/budgets' component={Budgets} />
+          <Match exactly pattern='/expenses' component={Expenses} />
+          <Match exactly pattern='/settings' component={Settings} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+})
+
+render(<App />, document.getElementById('app'))
--- js/NavBar.js
+++ js/NavBar.js
@@ -1,25 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 var NavBar = React.createClass({
-  render: function () {
+  render () {
     return (
-      <nav className="light-blue lighten-1" role="navigation">
-        <div className="nav-wrapper container">
-          <a id="logo-container" href="#" className="brand-logo">Dukikik</a>
-          <ul className="right hide-on-med-and-down">
-            <li><a href="#">Budgets</a></li>
-            <li><a href="#">Expenses</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
+      <nav className='light-blue lighten-1' role='navigation'>
+        <div className='nav-wrapper container'>
+          <Link to='/' id='logo-container' className='brand-logo'>Dukikik</Link>
+          <ul className='right hide-on-med-and-down'>
+            <li><Link to='/budgets'>Budgets</Link></li>
+            <li><Link to='/expenses'>Expenses</Link></li>
+            <li><Link to='/settings'>Settings</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
           </ul>
 
-          <ul id="nav-mobile" className="side-nav">
-            <li><a href="#">Budgets</a></li>
-            <li><a href="#">Expenses</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
+          <ul id='nav-mobile' className='side-nav'>
+            <li><Link to='/budgets'>Budgets</Link></li>
+            <li><Link to='/expenses'>Expenses</Link></li>
+            <li><Link to='/settings'>Settings</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
           </ul>
-          <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+          <a href='#' data-activates='nav-mobile' className='button-collapse'><i className='material-icons'>menu</i></a>
         </div>
       </nav>
     )
